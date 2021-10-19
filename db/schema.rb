b442cff77d3ab32a6e3c8526e6e7d635ff32c76e
codetalkertive <codetalkertive@gmail.com>
2021-10-19 19:36:43 +0100

    Add EventAttendance Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_18_135210) do
+ActiveRecord::Schema.define(version: 2021_10_18_145037) do
+
+  create_table "event_attendances", force: :cascade do |t|
+    t.integer "attendee_id"
+    t.integer "attended_event_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "name"
